1f47a5f38eae9ad835157df07f3f981e0a049948
AquilesIMM <aquilesmartinez490@gmail.com>
2026-01-18 15:17:36 -0300

    Menu basico Funcionando

--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -1,8 +1,10 @@
 package calculadora;
-
+import calculadora.Menu;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Calculadora iniciada");
+        Menu menu = new Menu();
+        menu.menu();
+
     }
 
 }
--- src/calculadora/Menu.java
+++ src/calculadora/Menu.java
@@ -9,67 +9,88 @@ public class Menu {
     public static void menu(){
         Scanner scanner = new Scanner(System.in);
         Calculadora calculadora = new Calculadora();
-        int opcion = scanner.nextInt();
-        String dato = scanner.next();
         List<Double> listaNumeros = new ArrayList<>();
+        int opcion = 0;
 
 
-        while(opcion != 5) {
+        while(opcion != 6) {
             System.out.println("CALCULADORA");
             System.out.println("1- Sumar");
             System.out.println("2- Restar");
             System.out.println("3- Multiplicar");
             System.out.println("4- Dividir");
-            System.out.println("5- Salir");
+            System.out.println("5- Limpiar");
+            System.out.println("6- Salir");
             System.out.print("Ingrese la opcion que desee: ");
+            opcion = scanner.nextInt();
+            String dato = ".";
 
 
 
             switch(opcion){
                 case 1:
-                    while (dato != "="){
-                        if (dato == "="){
-                            System.out.println(calculadora.sumar(listaNumeros);
+                    while (!dato.equals("=")){
+                        dato = scanner.next();
+                        if (dato.equals("=") ){
+                            double resultado = calculadora.sumar(listaNumeros);
+                            System.out.println(resultado);
                             break;
                         }else{
                             listaNumeros.add(Double.parseDouble(dato));
+                            System.out.println("+");
                         }
                     }
+                    break;
 
                 case 2:
-                    while (dato != "="){
-                        if (dato == "="){
-                            System.out.println(calculadora.restar(listaNumeros);
+                    while (!dato.equals("=")){
+                        dato = scanner.next();
+                        if (dato.equals("=") ){
+                            double resultado = calculadora.restar(listaNumeros);
+                            System.out.println(resultado);
                             break;
                         }else{
                             listaNumeros.add(Double.parseDouble(dato));
+                            System.out.println("-");
                         }
                     }
-
-                case 3:
-                    while (dato != "="){
-                        if (dato == "="){
-                            System.out.println(calculadora.multiplicar(listaNumeros);
-                            break;
-                        }else{
-                            listaNumeros.add(Double.parseDouble(dato));
-                        }
-                    }
-                case 4:
-                    while (dato != "="){
-                        if (dato == "="){
-                            System.out.println(calculadora.dividir(listaNumeros);
-                            break;
-                        }else{
-                            listaNumeros.add(Double.parseDouble(dato));
-                        }
-                    }
-                case 5:
-                    System.out.println("FIN DEL PROGRAMA");
                     break;
-                default:
-                    System.out.println("Opcion Incorrecta");
-            }
+                case 3:
+                    while (!dato.equals("=")){
+                        dato = scanner.next();
+                        if (dato.equals("=") ){
+                            double resultado = calculadora.multiplicar(listaNumeros);
+                            System.out.println(resultado);
+                            break;
+                        }else{
+                            listaNumeros.add(Double.parseDouble(dato));
+                            System.out.println("*");
+                        }
+                    }
+                    break;
+
+                case 4:
+                    while (!dato.equals("=")){
+                        dato = scanner.next();
+                        if (dato.equals("=") ){
+                            double resultado = calculadora.dividir(listaNumeros);
+                            System.out.println(resultado);
+                            break;
+                        }else{
+                            listaNumeros.add(Double.parseDouble(dato));
+                            System.out.println("/");
+                        }
+                    }
+                    break;
+                    case 5:
+                        listaNumeros.clear();
+                        break;
+                    case 6:
+                        System.out.println("FIN DEL PROGRAMA");
+                        break;
+                    default:
+                        System.out.println("Opcion Incorrecta");
+                }
 
         }
     }
