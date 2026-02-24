51b1e1e391049f817108f0319f6bdbadc111e991
AquilesIMM <aquilesmartinez490@gmail.com>
2026-02-24 14:03:12 -0300

    se agrega funcion para desplegar menu

--- src/calculadora/Menu.java
+++ src/calculadora/Menu.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 import calculadora.Calculadora;
-import calculadora.Herramientas;
+
 public class Menu {
 
     public static void menu(){
@@ -13,93 +13,85 @@ public class Menu {
         List<Double> listaNumeros = new ArrayList<>();
         int opcion = 0;
         double resultado = 0;
-        Herramientas herramientas = new Herramientas();
+        Menu menu = new Menu();
 
 
         while(opcion != 6) {
-            System.out.println("CALCULADORA");
-            System.out.println("1- Sumar");
-            System.out.println("2- Restar");
-            System.out.println("3- Multiplicar");
-            System.out.println("4- Dividir");
-            System.out.println("5- Limpiar");
-            System.out.println("6- Salir");
-            System.out.print("Ingrese la opcion que desee: ");
+            menu.printMenu();
             opcion = scanner.nextInt();
             String dato = ".";
             System.out.print(": ");
 
-
-
-            switch(opcion) {
-                case 1:
-                    if (!listaNumeros.isEmpty()) {
-                        listaNumeros = herramientas.validador(listaNumeros, resultado);
-                        System.out.print(" + ");
-                    }
-                    while (!dato.equals("=")) {
-                        dato = scanner.next();
-                        if (dato.equals("=")) {
-                            resultado = calculadora.sumar(listaNumeros);
-                            System.out.println(resultado);
-                            break;
-                        } else {
-                            listaNumeros.add(Double.parseDouble(dato));
+            try{
+                switch(opcion) {
+                    case 1:
+                        if (!listaNumeros.isEmpty()) {
+                            listaNumeros = menu.comprueba(listaNumeros, resultado);
                             System.out.print(" + ");
                         }
-                    }
-                    break;
-                case 2:
-                    if (!listaNumeros.isEmpty()) {
-                        listaNumeros = herramientas.validador(listaNumeros, resultado);
-                        System.out.print(" - ");
-                    }
-                    while (!dato.equals("=")){
-                        dato = scanner.next();
-                        if (dato.equals("=") ){
-                            resultado = calculadora.restar(listaNumeros);
-                            System.out.println(resultado);
-                            break;
-                        }else{
-                            listaNumeros.add(Double.parseDouble(dato));
-                            System.out.println("-");
+                        while (!dato.equals("=")) {
+                            dato = scanner.next();
+                            if (dato.equals("=")) {
+                                resultado = calculadora.sumar(listaNumeros);
+                                System.out.println(resultado);
+                                break;
+                            } else {
+                                listaNumeros.add(Double.parseDouble(dato));
+                                System.out.print(" + ");
+                            }
                         }
-                    }
-                    break;
-                case 3:
-                    if (!listaNumeros.isEmpty()) {
-                        listaNumeros = herramientas.validador(listaNumeros, resultado);
-                        System.out.print(" * ");
-                    }
-                    while (!dato.equals("=")){
-                        dato = scanner.next();
-                        if (dato.equals("=") ){
-                            resultado = calculadora.multiplicar(listaNumeros);
-                            System.out.println(resultado);
-                            break;
-                        }else{
-                            listaNumeros.add(Double.parseDouble(dato));
-                            System.out.println("*");
+                        break;
+                    case 2:
+                        if (!listaNumeros.isEmpty()) {
+                            listaNumeros = menu.comprueba(listaNumeros, resultado);
+                            System.out.print(" - ");
                         }
-                    }
-                    break;
-                case 4:
-                    if (!listaNumeros.isEmpty()) {
-                        listaNumeros = herramientas.validador(listaNumeros, resultado);
-                        System.out.print(" / ");
-                    }
-                    while (!dato.equals("=")){
-                        dato = scanner.next();
-                        if (dato.equals("=") ){
-                            resultado = calculadora.dividir(listaNumeros);
-                            System.out.println(resultado);
-                            break;
-                        }else{
-                            listaNumeros.add(Double.parseDouble(dato));
-                            System.out.println("/");
+                        while (!dato.equals("=")){
+                            dato = scanner.next();
+                            if (dato.equals("=") ){
+                                resultado = calculadora.restar(listaNumeros);
+                                System.out.println(resultado);
+                                break;
+                            }else{
+                                listaNumeros.add(Double.parseDouble(dato));
+                                System.out.println("-");
+                            }
                         }
-                    }
-                    break;
+                        break;
+                    case 3:
+                        if (!listaNumeros.isEmpty()) {
+                            listaNumeros = menu.comprueba(listaNumeros, resultado);
+                            System.out.print(" * ");
+                        }
+                        while (!dato.equals("=")){
+                            dato = scanner.next();
+                            if (dato.equals("=") ){
+                                resultado = calculadora.multiplicar(listaNumeros);
+                                System.out.println(resultado);
+                                break;
+                            }else{
+                                listaNumeros.add(Double.parseDouble(dato));
+                                System.out.println("*");
+                            }
+                        }
+                        break;
+                    case 4:
+                        if (!listaNumeros.isEmpty()) {
+                            listaNumeros = menu.comprueba(listaNumeros, resultado);
+                            System.out.print(" / ");
+                        }
+                        while (!dato.equals("=")){
+                            dato = scanner.next();
+                            if (dato.equals("=") ){
+                                resultado = calculadora.dividir(listaNumeros);
+                                System.out.println(resultado);
+                                break;
+                            }else{
+                                listaNumeros.add(Double.parseDouble(dato));
+                                System.out.println("/");
+                            }
+                        }
+                        break;
                     case 5:
                         listaNumeros.clear();
                         break;
@@ -108,10 +100,36 @@ public class Menu {
                         break;
                     default:
                         System.out.println("Opcion Incorrecta");
+                }
+            }catch()
+            a
+
+        {
+
             }
+
 
         }
     }
 
-}
+    public void  printMenu(){
+        System.out.println("CALCULADORA");
+        System.out.println("1- Sumar");
+        System.out.println("2- Restar");
+        System.out.println("3- Multiplicar");
+        System.out.println("4- Dividir");
+        System.out.println("5- Limpiar");
+        System.out.println("6- Salir");
+        System.out.print("Ingrese la opcion que desee: ");
 
+    }
+    public static List comprueba(List<Double> listaNumeros, double resultado) {
+        System.out.print(resultado);
+        listaNumeros.clear();
+        listaNumeros.add(resultado);
+        return listaNumeros;
+    }
+
+
+
+}
