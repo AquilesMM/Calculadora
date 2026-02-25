0766b33fd37b45cd29360b0931b5b1ded8e497a2
Agustin <agustin2k12@live.com>
2026-02-24 22:00:58 -0300

    empezando GUI y icono

--- src/calculadora/CalculadoraGUI.java
+++ src/calculadora/CalculadoraGUI.java
@@ -1,4 +1,22 @@
 package calculadora;
+import javax.swing.*;
+import java.awt.*;
 
 public class CalculadoraGUI {
+
+    static void main(String[] args) {
+
+        JFrame frame = new JFrame(); // frame
+        frame.setTitle("Calculadora");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true); // hacer frame visible
+        frame.setSize(400,400); // dimensiones en x, y
+        frame.setResizable(false); //no se puede ajustar pantalla
+        ImageIcon image = new ImageIcon("iconocalculadora.png"); // icono calculadora
+        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(Color.gray);
+
+
+    }
+
 }
